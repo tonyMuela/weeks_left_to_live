d4320660aff9e24b12fa7315d9c22c306ba42c38
Tony Muela <166438176+tonyMuela@users.noreply.github.com>
2024-06-07 10:29:40 -0700

    Create weeks_left_to_live.py

---
+++ weeks_left_to_live.py
@@ -0,0 +1,8 @@
+age = input("How old are you? ")
+
+years = 90 - int(age)
+weeks = years * 52
+days = weeks * 7
+
+print(f"If you live to be 90 you have about {weeks} weeks left.")
+print(f"That's about {days} more days.")
